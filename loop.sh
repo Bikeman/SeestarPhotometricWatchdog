4b4798b49198fdb3be27297630c4a5f34811b4b2
Heinz-Bernd Eggenstein <hb@tickuhr.de>
2025-03-23 16:04:39 +0100

    more initial editing

--- loop.sh
+++ loop.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-
 target=${1:-"T CrB"}
 nrstack=${2:-4}
 mincomps=${3:-3}
 waitnewframes=${4:-120}
+webdir=${5:-/var/www/htdocs/T_Cr}
+archive=${6:-/media/disk1/T_CrB/archive}
+
 
 while [[ 1 ]] ; do
 
@@ -15,7 +17,7 @@ if [ $? -eq 0 ]
 then
   ./stack_all.sh
   ./matcher.sh "$target" $mincomps
-  ./publish.sh
+  ./publish.sh "$5" "$6"
   echo "Digested new data at $(date -u)"
   sleep $waitnewframes
 else
--- publish.sh
+++ publish.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+webdir=${1}
+archive=${2}
+
 status=`tail -n 1 work/analysis/status_summary.csv | cut -f1 -d,`
 
-timeout -s 9 60  cp  work/analysis/checkplot.png  /media/share/www/htdocs/astro/T_CrB-latest.png
-timeout -s 9 60  cp  work/analysis/status_summary.csv /media/share/www/htdocs/astro/T_CrB-latest-status.csv
+timeout -s 9 60  cp  work/analysis/checkplot.png  $webdir/T_CrB-latest.png
+timeout -s 9 60  cp  work/analysis/status_summary.csv $webdir/T_CrB-latest-status.csv
 
-timeout -s 9 60  cp  work/analysis/checkplot.png  /media/share/www/htdocs/astro/T_CrB-latest-$status.png
-timeout -s 9 60  cp  work/analysis/status_summary.csv /media/share/www/htdocs/astro/T_CrB-latest-status-$status.csv
-
+timeout -s 9 60  cp  work/analysis/checkplot.png  $webdir/T_CrB-latest-$status.png
+timeout -s 9 60  cp  work/analysis/status_summary.csv $webdir/T_CrB-latest-status-$status.csv
 
 
 now=`date -u --iso-8601=seconds`
-timeout -s 9 30  cp work/analysis/checkplot.png /media/disk1/T_CrB/archive/plot-${now}.png
-timeout -s 9 30  cp work/analysis/status_summary.csv /media/disk1/T_CrB/archive/status-${now}.csv
+timeout -s 9 30  cp work/analysis/checkplot.png $archive/plot-${now}.png
+timeout -s 9 30  cp work/analysis/status_summary.csv $archive/status-${now}.csv
 
